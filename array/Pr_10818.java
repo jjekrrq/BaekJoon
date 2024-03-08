7eae0ba114ee870a07e5f17ea3f0f59964a185f7
Lee Seungchang <jjekrrq01@naver.com>
2024-03-08 10:58:57 +0900

    10818번 문제

--- array/Pr_10818.java
+++ array/Pr_10818.java
@@ -9,5 +9,26 @@ public class Pr_10818 {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer stringTokenizer;
+
+        String beforeParseTestCase = bufferedReader.readLine();
+        int testCase = Integer.parseInt(beforeParseTestCase);
+        int[] numList = new int[testCase];
+        stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+
+        for(int i = 0; i < testCase; i++){
+            numList[i] = Integer.parseInt(stringTokenizer.nextToken());
+        }
+        int max = numList[0];
+        int min = numList[0];
+
+        for(int i = 0; i < numList.length; i++){
+            if(max < numList[i]){
+                max = numList[i];
+            }
+            if(min > numList[i]){
+                min = numList[i];
+            }
+        }
+        System.out.println(min + " " + max);
     }
 }
