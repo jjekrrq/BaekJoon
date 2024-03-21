16403012206356f44350ebaac0e91ef50e0d9cb9
Lee Seungchang <jjekrrq01@naver.com>
2024-03-21 16:40:05 +0900

    1546번 문제 해결

--- array/Pr_1546.java
+++ array/Pr_1546.java
@@ -3,18 +3,22 @@ package baekjoon.array;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Pr_1546 {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer;
         String theNumberOfRepeatBefore = bufferedReader.readLine();
         int theNumberOfRepeat = Integer.parseInt(theNumberOfRepeatBefore);
         int[] totalSubjects = new int[theNumberOfRepeat];
         double sum = 0;
 
-        for(int i = 0; i < totalSubjects.length; i++){
-            String score = bufferedReader.readLine();
-            totalSubjects[i] = Integer.parseInt(score);
+        for(int i = 0; i < 1; i++){
+            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+            for(int j = 0; j < totalSubjects.length; j++){
+                totalSubjects[j] = Integer.parseInt(stringTokenizer.nextToken());
+            }
         }
         int biggest = totalSubjects[0];
         for(int i = 1; i < totalSubjects.length; i++){
