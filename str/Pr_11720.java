4d79673765834cbe7bc00fcce1232e7a123fbcdd
Lee Seungchang <jjekrrq01@naver.com>
2024-03-27 12:43:12 +0900

    해결 못한 부분 주석처리

---
+++ str/Pr_11720.java
@@ -0,0 +1,18 @@
+package baekjoon.str;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Pr_11720 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String num = bufferedReader.readLine();
+        String[] num1 = new String[num.length()];
+        int[] numList = new int[num1.length];
+
+        for(int i = 0; i < num1.length; i++){
+//            num1[i] = num.charAt(i);
+        }
+    }
+}
