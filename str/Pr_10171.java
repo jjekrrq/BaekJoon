7d553747755e92812644dbe2fd88e2229b1a23f3
Lee Seungchang <jjekrrq01@naver.com>
2026-01-04 16:33:35 +0900

    Pr_10171 : cats

---
+++ str/Pr_10171.java
@@ -0,0 +1,10 @@
+package baekjoon.str;
+
+public class Pr_10171 {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
